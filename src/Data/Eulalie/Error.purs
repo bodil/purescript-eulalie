ffa65012420182315576342e4ce656a913635497
Bodil Stokke <bodil@bodil.org>
2016-01-03 03:46:39 +0000

    Error printer.

--- src/Data/Eulalie/Error.purs
+++ src/Data/Eulalie/Error.purs
@@ -2,10 +2,12 @@ module Data.Eulalie.Error where
 
 import Prelude
 
-import Data.Set as Set
+import Data.List as List
 import Data.Set (Set())
+import Data.Set as Set
+import Data.String as String
 
-import Data.Eulalie.Stream (Stream(..))
+import Data.Eulalie.Stream (Stream(..), atEnd)
 
 type ParseError = { input :: Stream, expected :: Set String,
                     fatal :: Boolean }
@@ -22,3 +24,10 @@ extend e1@{ expected: x1, input: Stream s1 }
   if s1.cursor < s2.cursor then e2
   else if s1.cursor > s2.cursor then e1
        else e1 { expected = Set.union x1 x2 }
+
+print :: ParseError -> String
+print { input: input@Stream { buffer, cursor }, expected } =
+  "Expected " ++ exp expected ++ ", saw "
+  ++ (if atEnd input then "EOF" else quote $ String.take 6 $ String.drop cursor buffer) ++ "\n\"" ++ buffer ++ "\""
+  where quote s = "\"" ++ s ++ "\""
+        exp xs = String.joinWith " or " $ List.fromList $ Set.toList xs
